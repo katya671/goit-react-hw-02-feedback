ce5a1fecb39c2f76159e0f95360a60c69923a45f
Толочко Катерина <bantiktokinpakistan@gmail.com>
2023-08-03 22:13:29 +0300

    Done Step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  leaveFeedback = type => {
+    this.setState(prevState => ({
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <button onClick={() => this.leaveFeedback('good')}>Good</button>
+        <button onClick={() => this.leaveFeedback('neutral')}>Neutral</button>
+        <button onClick={() => this.leaveFeedback('bad')}>Bad</button>
+        <h2>Statistics</h2>
+        <ul>
+          <li>Good: {good}</li>
+          <li>Neutral: {neutral}</li>
+          <li>Bad: {bad}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
